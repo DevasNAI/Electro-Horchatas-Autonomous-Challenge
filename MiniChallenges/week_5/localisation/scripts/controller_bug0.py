532f965a23640a248794f86b47b1ff047a319bc1
MikeyFlores <FGJoseMiguel@outlook.com>
2024-05-26 21:48:43 -0600

    Add files via upload

---
+++ MiniChallenges/week_5/localisation/scripts/controller_bug0.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python3
+import rospy
+import math
+import tf
+import numpy as np
+from geometry_msgs.msg import Twist, PoseStamped, Pose
+from nav_msgs.msg import Odometry
+from std_msgs.msg import Float32
+from sensor_msgs.msg import LaserScan
+
+class Controller():
+    def __init__(self):
+        self.goal = PoseStamped()
+        self.goalSub = rospy.Subscriber("/move_base_simple/goal", PoseStamped, self.cbGoal)
+        
+        self.odom = Odometry()
+        self.odomSub = rospy.Subscriber('/puzzlebot_1/base_pose_ground_truth', Odometry, self.cbOdom)
+
+        self.kp_linear = 3.5
+        self.ki_linear = 2.0
+
+        self.kp_angular = 1.2
+        self.ki_angular = 0.7
+
+        self.goalPub = rospy.Publisher("/goal", PoseStamped, queue_size=1)
+        self.goal.header.frame_id = "world"
+        self.goal.pose.position.x = 0
+        self.goal.pose.position.y = -2.5
+
+        self.scan = LaserScan()
+        self.scanSub = rospy.Subscriber("/puzzlebot_1/scan", LaserScan, self.cbScan)
+
+        self.states = {
+            "STOP": self.stop,
+            "TOGOAL": self.controller,
+            "WALL": self.wall
+        }
+
+        self.currentState = "TOGOAL"
+
+    def cbGoal(self,goal_msg):
+        self.goal = goal_msg
+    
+    def cbOdom(self,odom_msg):
+        self.odom = odom_msg
+    
+    def cbScan(self,scan_msg):
+        self.scan = scan_msg
+        ranges = scan_msg.ranges
+        obstacle_distance = min(ranges)
+
+        error_x = self.goal.pose.position.x - self.odom.pose.pose.position.x
+        error_y = self.goal.pose.position.y - self.odom.pose.pose.position.y
+        error_linear = math.sqrt(error_x**2 + error_y**2)
+
+        if obstacle_distance < 0.30 and error_linear > 0.1:
+            self.currentState = "WALL"
+        elif error_linear < 0.1:
+            self.currentState = "STOP"
+        else:
+            self.currentState = "TOGOAL"
+        pass
+
+    def wrapToPi(self,theta):
+        return (theta + np.pi) % (2 * np.pi) - np.pi
+    
+    def stop(self):
+        # print("Stop")
+        cmd_vel_msg = Twist()
+        cmd_vel_msg.linear.x = 0.0
+        cmd_vel_msg.angular.z = 0.0
+        self.cmd_vel_pub.publish(cmd_vel_msg)
+    
+    def run(self):
+        rate = rospy.Rate(50)
+
+        # Create a publisher for the /cmd_vel topic
+        self.cmd_vel_pub = rospy.Publisher("/puzzlebot_1/base_controller/cmd_vel", Twist, queue_size=1)
+        
+        while not rospy.is_shutdown():
+            self.goalPub.publish(self.goal)
+            self.states[self.currentState]()
+            rate.sleep()
+
+    def controller(self):
+        theta = tf.transformations.euler_from_quaternion([self.odom.pose.pose.orientation.x, self.odom.pose.pose.orientation.y, self.odom.pose.pose.orientation.z, self.odom.pose.pose.orientation.w])[2]
+
+        # Calculate the error
+        error_x = self.goal.pose.position.x - self.odom.pose.pose.position.x
+        error_y = self.goal.pose.position.y - self.odom.pose.pose.position.y
+        error_linear = math.sqrt(error_x**2 + error_y**2)   
+
+        if (error_linear < 0.1):
+            self.stop()
+            return
+
+        error_Theta = math.atan2(error_y,error_x) - theta
+        if error_Theta > np.pi:
+            error_Theta = error_Theta - 2*np.pi
+        elif error_Theta < -np.pi:
+            error_Theta = error_Theta + 2*np.pi
+
+        # Calculate the control input
+        linear = self.kp_linear * error_linear + self.ki_linear * error_linear
+        angular = self.kp_angular * error_Theta + self.ki_angular * error_Theta            
+
+        # Publish the control input
+        cmd_vel_msg = Twist()
+        cmd_vel_msg.linear.x = linear
+        cmd_vel_msg.angular.z = angular
+
+        if cmd_vel_msg.linear.x < 0.01 and cmd_vel_msg.angular.z < 0.01:
+            return
+        
+        self.cmd_vel_pub.publish(cmd_vel_msg)
+        pass
+
+    def wall(self):
+        # print("Wall")
+        cmd_vel_msg = Twist()
+
+        # Get the minimum distance from the laser scan
+        ranges = self.scan.ranges
+        obstacle_distance = min(ranges)
+        min_index = ranges.index(obstacle_distance)
+        
+        # Calculate angle of the obstacle relative to the robot's orientation
+        angle_min = self.scan.angle_min  # Minimum angle of the laser scan
+        angle_increment = self.scan.angle_increment  # Increment between each range measurement
+        obstacle_angle = angle_min + (min_index * angle_increment)
+
+        diff = obstacle_angle - np.pi/2
+
+        # print("angle diff ", diff)
+
+        if diff < -0.05:
+            cmd_vel_msg.linear.x = 0.0
+            cmd_vel_msg.angular.z = -0.5
+            # print("turning right")
+        elif diff > 0.05:
+            cmd_vel_msg.linear.x = 0.0
+            cmd_vel_msg.angular.z = 0.5
+            # print("turning left")
+        else:
+            cmd_vel_msg.linear.x = 0.5
+            cmd_vel_msg.angular.z = 0.0
+            # print("moving forward")
+
+        self.cmd_vel_pub.publish(cmd_vel_msg)
+
+if __name__ == '__main__':
+    # Initialize the ROS node
+    rospy.init_node('control', anonymous=True)
+    Controller = Controller()
+    rospy.on_shutdown(Controller.stop)
+    controller.run()
---
+++ MiniChallenges/week_5/localisation/scripts/localisation.py
@@ -0,0 +1,87 @@
+import rospy
+import math
+from std_msgs.msg import Float32
+from nav_msgs.msg import Odometry
+import tf
+
+#!/usr/bin/env python2
+
+l = 0.19
+r = 0.05
+#starting position
+x = 4.0
+y = 4.0
+theta = -1.57
+wl = 0.0
+wr = 0.0
+def wr_cb(msg):
+    global wr
+    wr = msg.data
+    # rospy.loginfo("Right wheel velocity: %f", msg.data)
+def wl_cb(msg):
+    global wl
+    wl = msg.data
+    # rospy.loginfo("Left wheel velocity: %f", msg.data)
+
+def odometry_pub(wl, wr, dt):
+    global x, y, theta
+    # Calculate odometry based on wheel velocities
+    # Replace the following lines with your own odometry calculation logic
+    left_wheel_vel = wl
+    right_wheel_vel = wr
+
+    linear_vel = (left_wheel_vel + right_wheel_vel) / 2
+    angular_vel = r*(right_wheel_vel - left_wheel_vel) / 0.19
+    
+    theta += angular_vel*dt
+    linear_vel_x = linear_vel * math.cos(theta) 
+    linear_vel_y = linear_vel * math.sin(theta)
+
+    x += linear_vel_x*dt
+    y += linear_vel_y*dt
+
+    # Create odometry message
+    odom_msg = Odometry()
+    odom_msg.header.stamp = rospy.Time.now()
+    odom_msg.header.frame_id = 'world'
+    odom_msg.child_frame_id = '/base_link'
+
+    odom_msg.pose.pose.position.x = x
+    odom_msg.pose.pose.position.y = y
+    odom_msg.pose.pose.position.z = 0.0
+    quaternion = tf.transformations.quaternion_from_euler(0, 0, theta)
+    odom_msg.pose.pose.orientation.x = quaternion[0]
+    odom_msg.pose.pose.orientation.y = quaternion[1]
+    odom_msg.pose.pose.orientation.z = quaternion[2]
+    odom_msg.pose.pose.orientation.w = quaternion[3]
+
+    odom_msg.twist.twist.linear.x = linear_vel
+    odom_msg.twist.twist.angular.z = angular_vel
+
+    # Publish odometry message
+    odom_pub.publish(odom_msg)
+
+if __name__ == '__main__':
+    rospy.init_node('localisation_node')
+
+    # Subscribe to wheel velocity topics
+    rospy.Subscriber('/puzzlebot_1/wl', Float32, wl_cb)
+    rospy.Subscriber('/puzzlebot_1/wr', Float32, wr_cb)
+
+    # Create odometry publisher
+    odom_pub = rospy.Publisher('/odom', Odometry, queue_size=10)
+
+    rate = rospy.Rate(25) # 100 Hz
+    prev_time = rospy.Time.now().to_sec()
+
+    while not rospy.is_shutdown():
+        # Calculate time difference
+        currentTime = rospy.Time.now().to_sec()
+        dt = currentTime - prev_time
+        prev_time = currentTime
+
+        # Publish odometry message
+        odometry_pub(wl, wr, dt)
+
+        # Sleep to maintain the rate
+        rate.sleep()
